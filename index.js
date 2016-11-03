291af40503de480ff83a5f15475a702ef8d42f7e
alincode <alincode@gmail.com>
2016-11-03 14:05:20 +0800

    init version

---
+++ index.js
@@ -0,0 +1,8 @@
+import Promise from "bluebird";
+import _ from "lodash";
+require("babel-core/register");
+require("babel-polyfill");
+
+global.Promise = Promise;
+global._ = _;
+module.exports.product = require('./lib/product');
---
+++ test/field.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ProductFields: [
+    'sku', 'amount', 'mfs', 'pn', 'desc', 'leadAndRohs', 'attrs', 'docs',
+    'prices', 'url'
+  ]
+};
